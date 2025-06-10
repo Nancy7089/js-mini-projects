10a8f9511b2eeceee55f7bf0ce6a823128529ab9
Nancy7089 <151985849+Nancy7089@users.noreply.github.com>
2025-06-10 21:58:06 +0530

    Delete ToDo List directory

--- ToDo List/todo-list.js
+++
@@ -1,9 +0,0 @@
-const todoList=[];
-
-function addTodo(){
-  const inputEle=document.querySelector('.js-name-input');
-  const name=inputEle.ariaValueMax;
-  todoList.push(name);
-  console.log(todoList);
-  inputEle.value='';
-}
